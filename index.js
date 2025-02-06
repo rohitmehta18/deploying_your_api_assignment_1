eeda79dc67e5beb3918cf134b1963fd75b63fba9
rohitmehta18 <rmehta1836@gmail.com>
2025-02-06 11:29:16 +0530

    update

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
+require('dotenv').config()
+
+const config = {
+  apiKey: process.env.API_KEY,
+
+  serverSecret: process.env.SERVER_SECRET,
+
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+console.log(config)
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
